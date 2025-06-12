2bd56fb5a3c250600c4eafdadf0f2552326d398e
joschif <jonas.simon.fleck@gmail.com>
2025-06-12 13:32:21 +0200

    build: add support for simple dispersion methods

--- src/delnx/tl/_jax_tests.py
+++ src/delnx/tl/_jax_tests.py
@@ -220,19 +220,24 @@ def _estimate_dispersion_batched(
     """
     n_features = X.shape[1]
     estimator = DispersionEstimator(**kwargs)
+    estimation_method = "mle" if method in ["mle", "deseq2"] else "moments"
 
     # Batched estimation of initial dispersion
     init_dispersions = []
     for i in tqdm.tqdm(range(0, n_features, batch_size), disable=not verbose):
         batch = slice(i, min(i + batch_size, n_features))
         X_batch = jnp.asarray(_to_dense(X[:, batch]), dtype=jnp.float32)
-        dispersion = estimator.estimate_dispersion(X_batch, method=method)
+        dispersion = estimator.estimate_dispersion(X_batch, method=estimation_method)
         init_dispersions.append(dispersion)
 
     init_dispersions = jnp.concatenate(init_dispersions, axis=0)
-    mean_counts = jnp.mean(X, axis=0)
+
+    if method in ["mle", "moments"]:
+        # If using MLE or moments, return initial estimates directly
+        return init_dispersions
 
     # Shrinkage of dispersion towards trend
+    mean_counts = jnp.mean(X, axis=0)
     dispersions = estimator.shrink_dispersion(
         dispersions=init_dispersions,
         mu=mean_counts,
