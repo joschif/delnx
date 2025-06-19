a4b7500f5178c6da15cf81e075965340228c5f3b
joschif <jonas.simon.fleck@gmail.com>
2025-06-19 10:22:22 +0200

    fix: dont remove group col in grouped DE

--- src/delnx/tl/_de.py
+++ src/delnx/tl/_de.py
@@ -183,6 +183,7 @@ def _grouped_de(
             "feature",
             "test_condition",
             "ref_condition",
+            "group",
             "log2fc",
             "auroc",
             "coef",
