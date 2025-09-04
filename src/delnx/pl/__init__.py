99a99af922ee2f26885ad5fc7b9976398b2927de
joschif <jonas.simon.fleck@gmail.com>
2025-09-04 11:44:22 +0200

    cleanup: fix imports

--- src/delnx/pl/__init__.py
+++ src/delnx/pl/__init__.py
@@ -1,6 +1,4 @@
 from ._dotplot import DotPlot, dotplot
-from ._filtered_genes import plot_filtered_genes
-from ._gseaplot import gsea_barplot, gsea_dotplot, gsea_heatmap
 from ._heatmapplot import heatmap
 from ._matrixplot import MatrixPlot, matrixplot
 from ._violinplot import ViolinPlot, violinplot
